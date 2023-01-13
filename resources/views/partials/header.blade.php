e1ca84f6c40d62a365324b9168a60eba2f959737
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-13 18:07:42 +0100

    Modifica ul

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,13 +3,16 @@
         <img src="{{Vite::asset("resources/img/dc-logo.png")}}">
         <nav class="ms-nav">
             <ul class="d-flex list-unstyled">
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
-                <li><a href=""></a>Characters</li>
+                <li><a class="menu-item" href="#"></a>CHARACTERS</li>
+                <li><a class="menu-item" href="#"></a>COMICS</li>
+                <li><a class="menu-item" href="#"></a>MOVIES</li>
+                <li><a class="menu-item" href="#"></a>TV</li>
+                <li><a class="menu-item" href="#"></a>GAMES</li>
+                <li><a class="menu-item" href="#"></a>COLLECTIBLESS</li>
+                <li><a class="menu-item" href="#"></a>VIDEOS</li>
+                <li><a class="menu-item" href="#"></a>FANS</li>
+                <li><a class="menu-item" href="#"></a>NEWS</li>
+                <li><a class="menu-item" href="#"></a>SHOP</li>
             </ul>
         </nav>
         <div class="search">
