33d67e68c46e7daef32b4a87422a1fb90b965558
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-13 17:47:44 +0100

    Inserisce header

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page-title')
+    Homepage
+@endsection
+
+@section('page-content')
+    <section>
+        <h2>Lista</h2>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="container d-flex justify-content-between align-items-center">
+        <img src="{{Vite::asset("resources/img/dc-logo.png")}}">
+        <nav class="ms-nav">
+            <ul class="d-flex list-unstyled">
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+                <li><a href=""></a>Characters</li>
+            </ul>
+        </nav>
+        <div class="search">
+            <span>Search</span>
+        </div>
+    </div>
+</header>
