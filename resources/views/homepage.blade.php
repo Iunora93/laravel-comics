6eb2d2691edfc4e982e7d985dc1977ff8447b32b
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-16 14:52:35 +0100

    Aggiunge card dinamiche

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,54 @@
-@extends('layouts.main')
-
+@extends('layouts.main');
 @section('page-title')
-    Homepage
+   Homepage 
 @endsection
+@section("page-content")
+<div class="my-bg">
+    <section class="container">
+        <div class="comics">
+            <button class="btn-series">
+                <h2>CURRENT SERIES</h2>
+            </button>
 
-@section('page-content')
-    <section>
-        <h2>Lista</h2>
+            @foreach ($comics as $comic)
+            <div class="comic" >
+                <img src="{{$comic['thumb']}}" alt="">
+                <h5>{{$comic['series']}}</h5>
+            </div>
+            @endforeach
+                
+        </div>
+        <div class="d-flex">
+            <button class="btn-load">
+                LOAD MORE
+            </button>
+        </div>
     </section>
+</div>
+
+<section class="ads-section">
+   <div class="container">
+   <div>
+      <img src="{{Vite::asset("resources/img/buy-comics-digital-comics.png")}}">
+      <span>Digital Comics</span>
+    </div>
+    <div>
+      <img src="{{Vite::asset("resources/img/buy-comics-merchandise.png")}}"/>
+      <span>DC Merchandise</span>
+    </div>
+    <div>
+      <img src="{{Vite::asset("resources/img/buy-comics-shop-locator.png")}}" />
+      <span>Subscription</span>
+    </div>
+    <div>
+      <img src="{{Vite::asset("resources/img/buy-comics-subscriptions.png")}}" />
+      <span>Comic shop locator</span>
+    </div>
+    <div>
+      <img src="{{Vite::asset("resources/img/buy-dc-power-visa.svg")}}" />
+      <span>Dc Power visa</span>
+    </div>
+   </div>
+</section>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <div class="container d-flex justify-content-between align-items-center">
+    <div class="container d-flex justify-content-between align-items-center mb-2">
         <img src="{{Vite::asset("resources/img/dc-logo.png")}}">
-        <nav class="ms-nav">
+        <nav>
             <ul class="d-flex list-unstyled">
                 <li><a class="menu-item" href="#"></a>CHARACTERS</li>
                 <li><a class="menu-item" href="#"></a>COMICS</li>
@@ -15,8 +15,11 @@
                 <li><a class="menu-item" href="#"></a>SHOP</li>
             </ul>
         </nav>
-        <div class="search">
-            <span>Search</span>
+        <div>
+        <span>Search</span>
+            <i class="fa-regular fa-magnifying-glass"></i>
         </div>
-    </div>
+    </div>    
 </header>
+<div class="hero">
+</div>
